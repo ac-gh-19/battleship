0498c15d1df660aa018866244c84bba15634dce1
ac-gh-19 <chanandrew1925@gmail.com>
2025-10-11 19:47:32 -0700

    Feat: Add GameController with tests
    Implements the GameController class which manages
    the flow of the game. It handles player turns,
    makes attacks on the opponent's board, checks for
    when the game is over, and initalizes the game
    with two players.

---
+++ src/app/gameController.js
@@ -0,0 +1,45 @@
+export default class GameController {
+  constructor(player1, player2) {
+    this.player1 = player1;
+    this.player2 = player2;
+    this.currentPlayer = player1;
+    this.winner = null;
+  }
+
+  // who is the current Player?
+  // switch to the other player
+  switchTurn() {
+    this.currentPlayer === this.player1
+      ? (this.currentPlayer = this.player2)
+      : (this.currentPlayer = this.player1);
+  }
+
+  // opponent
+  makeMove([x, y]) {
+    let opponent;
+    this.currentPlayer === this.player1
+      ? (opponent = this.player2)
+      : (opponent = this.player1);
+
+    if (this.currentPlayer.attack(opponent, [x, y])) {
+      if (this.gameOver()) {
+        this.winner = this.currentPlayer;
+        return "GAME OVER";
+      }
+
+      this.switchTurn();
+      return true;
+    }
+    return false;
+  }
+
+  // if all player 1 all ships sunk or
+  // player 2 all ships sunk then game over
+  // winner will be currentPlayer bec most recent attack
+  gameOver() {
+    return (
+      this.player1.gameboard.allShipsSunk() ||
+      this.player2.gameboard.allShipsSunk()
+    );
+  }
+}
---
+++ tests/gameController.test.js
@@ -0,0 +1,79 @@
+import GameController from "../src/app/gameController.js";
+import Player from "../src/app/player.js";
+import Ship from "../src/app/ship.js";
+
+// We've already tested the Player, Ship, and Gameboard functions individually.
+// In theory, this means the GameController functions should work since they
+// rely on those modules and just call upon those functions. However, we're
+// testing the GameController as well to ensure they integrate correctly.
+
+test("Game Setup Initializes Two Player Instances", () => {
+  let player1 = new Player("AC", "Human");
+  let player2 = new Player("CPU", "Computer");
+  let game = new GameController(player1, player2);
+  expect(game.player1).toBe(player1);
+  expect(game.player2).toBe(player2);
+});
+
+test("Switches Turns Between Players", () => {
+  let player1 = new Player("AC", "Human");
+  let player2 = new Player("CPU", "Computer");
+  let game = new GameController(player1, player2);
+
+  expect(game.currentPlayer).toBe(player1);
+  game.switchTurn();
+  expect(game.currentPlayer).toBe(player2);
+  game.switchTurn();
+  expect(game.currentPlayer).toBe(player1);
+});
+
+test("Invalid Move Does Not Switch Turn", () => {
+  let player1 = new Player("AC", "Human");
+  let player2 = new Player("CPU", "Computer");
+  let game = new GameController(player1, player2);
+
+  game.makeMove([0, 0]);
+  // manually switch currentPlayer to player1 again
+  // because makeMove switches other player at end
+  game.currentPlayer = player1;
+  expect(game.makeMove([0, 0])).toBe(false);
+  expect(game.currentPlayer).toBe(player1);
+});
+
+test("Attacking Affects Opponent's Board", () => {
+  let player1 = new Player("AC", "Human");
+  let player2 = new Player("CPU", "Computer");
+  let game = new GameController(player1, player2);
+
+  game.makeMove([0, 0]);
+  expect(player2.gameboard.board[0][0].isAttacked).toBe(true);
+
+  // current player should now be player2 as
+  // makeMove calls switchTurn() at the end
+  game.makeMove([0, 0]);
+  expect(player1.gameboard.board[0][0].isAttacked).toBe(true);
+});
+
+test("Prevents Attacking the Same Position", () => {
+  let player1 = new Player("AC", "Human");
+  let player2 = new Player("CPU", "Computer");
+  let game = new GameController(player1, player2);
+
+  game.makeMove([0, 0]);
+  // manually switch currentPlayer to player1 again
+  // because makeMove switches other player at end
+  game.currentPlayer = player1;
+  expect(game.makeMove([0, 0])).toBe(false);
+});
+
+test("Detects When Game is or Isn't Over", () => {
+  let player1 = new Player("AC", "Human");
+  let player2 = new Player("CPU", "Computer");
+  let game = new GameController(player1, player2);
+
+  player1.gameboard.placeShip(new Ship(1), [0, 0], "horizontal");
+  player2.gameboard.placeShip(new Ship(1), [0, 0], "horizontal");
+  expect(game.gameOver()).toBe(false);
+  game.makeMove([0, 0]);
+  expect(game.gameOver()).toBe(true);
+});
