374d250545a3b357d064d50ac421f441eac06e29
ac-gh-19 <chanandrew1925@gmail.com>
2025-10-23 03:11:21 -0700

    Add Multiple Turns if Player Hits Ship

--- src/app/loadGameController.js
+++ src/app/loadGameController.js
@@ -6,7 +6,12 @@ export function loadGame(playerOne, playerTwo) {
   let app = document.querySelector("#app");
   let game = new GameController(playerOne, playerTwo);
 
-  let { gameInfo, p1Board, p2Board } = renderGameLayout(app, playerOne, playerTwo, createGame);
+  let { gameInfo, p1Board, p2Board } = renderGameLayout(
+    app,
+    playerOne,
+    playerTwo,
+    createGame,
+  );
 
   renderBoards(playerOne, playerTwo);
   renderGameInfo(gameInfo, game);
@@ -14,30 +19,37 @@ export function loadGame(playerOne, playerTwo) {
   p1Board.addEventListener("click", (e) => {
     if (game.currentPlayer != playerTwo) return;
     let [x, y] = [e.target.dataset.x, e.target.dataset.y];
-    playMove(game, playerTwo, playerOne, [x,y], gameInfo)
-  })
+    playMove(game, playerTwo, playerOne, [x, y], gameInfo);
+  });
   p2Board.addEventListener("click", (e) => {
     if (game.currentPlayer != playerOne) return;
     let [x, y] = [e.target.dataset.x, e.target.dataset.y];
-    playMove(game, playerOne, playerTwo, [x,y], gameInfo)
+    playMove(game, playerOne, playerTwo, [x, y], gameInfo);
 
-    if (playerTwo.type == "Computer" && !game.winner) {
+    if (playerTwo.type == "Computer" && !game.winner && game.currentPlayer == playerTwo) {
       setTimeout(() => {
-        playMove(game, playerOne, playerTwo, [x,y], gameInfo);
-      }, 1000)
+          playMove(game, playerTwo, playerOne, [x, y], gameInfo);
+      }, 750);
     }
   });
 }
 
-function playMove(game, attacker, opponent, [x,y], gameInfo) {
+function playMove(game, attacker, opponent, [x, y], gameInfo) {
   if (attacker.type == "Computer") {
     do {
-      ({x, y} = attacker.gameboard.getRandMove());
-    } while (!game.makeMove([x,y]));
+      ({ x, y } = attacker.gameboard.getRandMove());
+    } while (!game.makeMove([x, y]));
+    renderBoards(attacker, opponent);
+    renderGameInfo(gameInfo, game);
+
+    if (game.currentPlayer == attacker) {
+      setTimeout(() => {
+        playMove(game, attacker, opponent, [x, y], gameInfo);
+      }, 500);
+    }
   } else {
-    game.makeMove([x,y]);
+    game.makeMove([x, y]);
   }
   renderBoards(attacker, opponent);
   renderGameInfo(gameInfo, game);
-};
-
+}
